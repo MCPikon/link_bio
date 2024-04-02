c2c36075921037d833a2c360985ef205d51de166
MCPikon <javierpiconpastor@gmail.com>
2024-04-02 19:11:53 +0200

    minor changes

--- link_bio/link_bio.py
+++ link_bio/link_bio.py
@@ -2,4 +2,7 @@ import reflex as rx
 import link_bio.styles.styles as styles
 from link_bio.pages import index
 
+# class State(rx.State):
+#     pass
+
 app = rx.App(stylesheets=styles.STYLESHEETS, style=styles.BASE_STYLE)
