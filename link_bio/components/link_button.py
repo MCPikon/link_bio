c72ae7b25a4e9692948d978b38b165a26dbcf5a0
MCPikon <javierpiconpastor@gmail.com>
2024-04-02 19:33:53 +0200

    change rx.redirect for rx.link

--- link_bio/components/link_button.py
+++ link_bio/components/link_button.py
@@ -5,32 +5,35 @@ from link_bio.styles.styles import Size as Size, Spacing
 
 def link_button(title: str, body: str, img_src: str, url: str) -> rx.Component:
     return rx.button(
-        rx.hstack(
-            rx.image(
-                src=img_src, 
-                width=Size.LARGE.value,
-                height=Size.LARGE.value,
-                margin=Size.MEDIUM.value,
-                alt=title
-            ),
-            rx.vstack(
-                rx.text(
-                    title, 
-                    size=Spacing.SMALL.value,
-                    style=styles.button_title_style
+        rx.link(
+            rx.hstack(
+                rx.image(
+                    src=img_src,
+                    width=Size.LARGE.value,
+                    height=Size.LARGE.value,
+                    margin=Size.MEDIUM.value,
+                    alt=title
                 ),
-                rx.text(
-                    body, 
-                    size=Spacing.SMALLER.value,
-                    style=styles.button_body_style
+                rx.vstack(
+                    rx.text(
+                        title,
+                        size=Spacing.SMALL.value,
+                        style=styles.button_title_style
+                    ),
+                    rx.text(
+                        body,
+                        size=Spacing.SMALLER.value,
+                        style=styles.button_body_style
+                    ),
+                    align_items="start",
+                    spacing=Spacing.VERY_SMALL.value,
+                    padding_y=Size.SMALL.value,
+                    padding_right=Size.SMALL.value
                 ),
-                align_items="start",
-                spacing=Spacing.VERY_SMALL.value,
-                padding_y=Size.SMALL.value,
-                padding_right=Size.SMALL.value
+                align="center",
+                width="100%"
             ),
-            align="center",
-            width="100%"
-        ),
-        on_click=rx.redirect(path=url, external=True)
+            href=url,
+            is_external=True
+        )
     )
